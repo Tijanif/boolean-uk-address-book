ee860a272b5fbd297798cd44cbbee99099d29109
Tijani Fulani <Fulani07@gmail.com>
2021-05-25 17:35:54 +0100

    .

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const contactsSection = document.querySelector('.contacts-section');
 const state = {
   contacts: [],
   selectedContact: null,
+  // addresses: [],
 };
 
 /* [START] NO NEED TO EDIT */
@@ -23,7 +24,6 @@ function getContacts() {
 function renderContactsList() {
   const listEl = document.createElement('ul');
   listEl.className = 'contacts-list';
-
   for (let i = 0; i < state.contacts.length; i++) {
     const contact = state.contacts[i];
     const listItemEl = renderContactListItem(contact);
@@ -125,11 +125,10 @@ function renderContactListItem(contact) {
 function listenNewContactButton() {
   const btn = document.querySelector('.new-contact-btn');
 
-  btn.addEventListener('click', function () {
-    // [TODO] Write Code
+  btn.addEventListener('click', function (e) {
+    e.preventDefault();
     viewSection.innerHTML = '';
     const contactForm = renderFormEl();
-
     viewSection.append(contactForm);
   });
 }
@@ -160,59 +159,83 @@ const renderFormEl = () => {
   firstNameLabel.innerText = 'First Name:';
   // first name input
   const firstNameInput = createEl('input', {
-    id: 'firstname',
+    id: 'firstName',
     name: 'first-name-input',
   });
   // firstNameInput.setAttribute('id', 'firstname');
   // firstNameInput.setAttribute('name', 'first-name-input', 'type', 'text');
   // labe for last name
-  const lastNameLabel = createEl('label');
-  lastNameLabel.setAttribute('for', 'last-name-input');
+  const lastNameLabel = createEl('label', {
+    for: 'last-name-input',
+  });
+  // lastNameLabel.setAttribute('for', 'last-name-input');
   lastNameLabel.innerText = 'Last Name:';
 
   // input last name
-  const lastNameInput = createEl('input');
-  lastNameInput.setAttribute('id', 'lastname');
-  lastNameInput.setAttribute('name', 'last-name-input', 'type', 'text');
+  const lastNameInput = createEl('input', {
+    id: 'lastName',
+    name: 'last-name-input',
+  });
+  // lastNameInput.setAttribute('id', 'lastname');
+  // lastNameInput.setAttribute('name', 'last-name-input', 'type', 'text');
   // street label
-  const streetLabel = createEl('labe');
-  streetLabel.setAttribute('for', 'street-input');
+  const streetLabel = createEl('label', {
+    for: 'label',
+  });
+  // streetLabel.setAttribute('for', 'street-input');
   streetLabel.innerText = 'Street:';
 
   // street input
-  const streetInput = createEl('input');
-  streetInput.setAttribute('id', 'street-input');
-  streetInput.setAttribute('name', 'street', 'type', 'text');
+  const streetInput = createEl('input', {
+    id: 'street',
+    name: 'street',
+  });
+  // streetInput.setAttribute('id', 'street-input');
+  // streetInput.setAttribute('name', 'street', 'type', 'text');
 
   // city label
-  const cityLabel = createEl('label');
-  cityLabel.setAttribute('for', 'city-input');
+  const cityLabel = createEl('label', {
+    for: 'city-input',
+  });
+  // cityLabel.setAttribute('for', 'city-input');
   cityLabel.innerText = 'City:';
 
   // city input
-  const cityInput = createEl('input');
-  cityInput.setAttribute('id', 'city-input');
-  cityInput.setAttribute('name', 'city', 'type', 'text');
+  const cityInput = createEl('input', {
+    id: 'city',
+    name: 'city',
+  });
+  // cityInput.setAttribute('id', 'city-input');
+  // cityInput.setAttribute('name', 'city', 'type', 'text');
 
   // postCode label
-  const postCodeLabel = createEl('label');
-  postCodeLabel.setAttribute('for', 'post-code-input');
+  const postCodeLabel = createEl('label', {
+    for: 'post-code-inpu',
+  });
+  // postCodeLabel.setAttribute('for', 'post-code-input');
   postCodeLabel.innerText = 'Post Code:';
 
   // postCode input
-  const postCodeInput = createEl('input');
-  postCodeInput.setAttribute('id', 'post-code-input');
-  postCodeInput.setAttribute('name', 'postCode');
+  const postCodeInput = createEl('input', {
+    id: 'postCode',
+    name: 'postCode',
+  });
+  // postCodeInput.setAttribute('id', 'post-code-input');
+  // postCodeInput.setAttribute('name', 'postCode');
   postCodeInput.setAttribute('type', 'text');
 
   // Checkbox section
   const checkboxSectionEL = createEl('div');
   checkboxSectionEL.setAttribute('class', 'checkbox-section');
 
-  const checkboxInput = createEl('input');
-  checkboxInput.setAttribute('id', 'block-checkbox');
-  checkboxInput.setAttribute('type', 'checkbox');
-  checkboxInput.setAttribute('name', 'block-checkbox');
+  const checkboxInput = createEl('input', {
+    id: 'block-checkbox',
+    name: 'block-checkbox',
+    type: 'checkbox',
+  });
+  // checkboxInput.setAttribute('id', 'block-checkbox');
+  // checkboxInput.setAttribute('type', 'checkbox');
+  // checkboxInput.setAttribute('name', 'block-checkbox');
 
   // checkbox label
 
@@ -247,21 +270,54 @@ const renderFormEl = () => {
     actionsSection
   );
 
-  // formEl.addEventListener('submit', submitForm)
-  // {
-  // }
+  formEl.addEventListener('submit', function (e) {
+    e.preventDefault();
 
-  const submitForm = (event) => {
-    event.preventDefault();
-
-    const { firstName, lastName, city, street } = e.value;
-
-    const contact = {
-      firstName: firstName.value,
-      LastName: lastName.value,
-      blockContact: true,
+    const addAddress = {
+      id: state.addresses + 1,
+      street: formEl.street.value,
+      city: formEl.city.value,
+      postCode: formEl.postCode.value,
     };
-  };
+    const contactInfo = {
+      firstName: formEl.firstName.value,
+      lastName: formEl.lastName.value,
+      blockContact: !formEl['block-checkbox'].value,
+      addressId: null,
+    };
+
+    fetch('http://localhost:3000/addresses', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(addAddress),
+    })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        contactInfo.addressId = data.id;
+
+        fetch('http://localhost:3000/contacts', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(contactInfo),
+        })
+          .then(function (response) {
+            return response.json();
+          })
+          .then(function (data) {
+            state.contacts.push(data);
+          });
+        const contactsEl = document.querySelector('.contacts-list');
+        contactsEl.innerHTML = '';
+        main();
+      });
+  });
+
   return formEl;
 };
 
@@ -274,6 +330,7 @@ renderFormEl();
 // [TODO] Write Code
 
 function main() {
+  // listEl.innerHTML = '';
   listenNewContactButton();
   getContacts();
 }
